24ca1de5c51f5a8c0aa06a6539032ef3d7906a00
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-07-17 23:26:10 -0500

    Add title, buttons and profile image

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
