24a917058334c0486898688d28f8e9d6c03388f1
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-10-31 16:30:21 -0500

    Validate screen size

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Project } from "../../models/project";
 import { ActivatedRoute, Params } from "@angular/router";
@@ -13,18 +13,33 @@ import { ActivatedRoute, Params } from "@angular/router";
 export class ProjectComponent implements OnInit {
 
   project: Project;
+  isDesktopScreen: boolean;
+  screenSize: number | undefined;
 
   constructor(private route: ActivatedRoute) {
     this.project = {} as Project;
+    this.isDesktopScreen = true;
   }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(
       (params: Params) => {
-        if (params['project']) {
-          this.project = JSON.parse(params['project']);
-        }
+        if (params['project']) this.project = JSON.parse(params['project']);
       }
     )
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.isDesktopScreen = this.checkScreenSize();
+    console.log(this.screenSize)
+  }
+
+  checkScreenSize(): boolean {
+    this.screenSize = document.getElementById("screen")?.offsetWidth;
+
+    if (this.screenSize && this.screenSize > 700) return true;
+
+    return false;
   }
 }
