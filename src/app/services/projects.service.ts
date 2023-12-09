7316a438f39c8e0f8998173c726382cdcddc8ee6
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-12-09 12:04:24 -0500

    Add save method
    Send POST request to save a Project.

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -2,6 +2,8 @@ import { Injectable, signal, WritableSignal } from '@angular/core';
 import { Project } from "../models/project";
 import { BehaviorSubject } from "rxjs";
 import { Color } from "../models/color";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment.development";
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,12 @@ export class ProjectsService {
   currentProject: WritableSignal<Project>;
   currentProjectColors: BehaviorSubject<[Color, Color]>;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.currentProject = signal({} as Project);
     this.currentProjectColors = new BehaviorSubject<[Color, Color]>([{} as Color, {} as Color]);
+  }
+
+  saveProject(project: Project) {
+    return this.http.post<Project>(`${environment.BASE_URL}/${environment.PROJECTS_ENDPOINT}`, project);
   }
 }
