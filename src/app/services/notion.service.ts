9ba8452f7ce256e7d62600b9cc771771d2094060
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2024-03-28 15:17:06 -0500

    Reduce code

--- src/app/services/notion.service.ts
+++ src/app/services/notion.service.ts
@@ -9,15 +9,10 @@ import { Pages } from "../models/pages";
 })
 export class NotionService {
 
-  private readonly baseUrl: string;
-  private readonly notionEndpoint: string;
-
   constructor(private http: HttpClient) {
-    this.baseUrl = environment.BASE_URL;
-    this.notionEndpoint = environment.NOTION_ENDPOINT;
   }
 
   getPages(): Observable<Pages> {
-    return this.http.get<Pages>(`${this.baseUrl}/${this.notionEndpoint}`);
+    return this.http.get<Pages>(`${environment.BASE_URL}/${environment.NOTION_ENDPOINT}`);
   }
 }
