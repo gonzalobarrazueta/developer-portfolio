15267febe59adc984c725f73f884a34763166f4d
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2024-09-21 23:22:13 -0500

    Add project list

---
+++ src/app/components/projects/project-list.ts
@@ -0,0 +1,12 @@
+export const PROJECTS = [
+  {
+    pageId: 1,
+    title: "Spotify Music Recommendations App",
+    description: "Aplicación web que hace uso de la API de Spotify para brindarte sugerencias musicales basadas en tus canciones más reproducidas. Después de iniciar sesión con tu cuenta de Spotify, podrás selecciona cualquier canción, escucharla y agregarla a una playlist.",
+    technologies: ["Angular", "TypeScript"],
+    imageUrl: "assets/images/projects/music_recommendations_project.png",
+    demo: "https://www.youtube.com/embed/eFpPRCmymuw?si=do0YsdPfBLE5HDTN",
+    github: "https://github.com/gonzalobarrazueta/song-recommendations",
+    live: "https://musicloop.netlify.app/"
+  }
+];
