22ff092925a4e8856713f3014b089954dafb25fc
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-03-15 21:17:28 -0500

    Add padding and gradient background to footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ProjectColorsService} from "../../shared/project-colors.service";
 
 @Component({
   selector: 'app-footer',
@@ -10,4 +11,9 @@ import { CommonModule } from '@angular/common';
 })
 export class FooterComponent {
 
+  gradientBackground: string;
+
+  constructor(private colors: ProjectColorsService) {
+    this.gradientBackground = "background: linear-gradient(180deg, #FFFAE5 0%, #FFF2BD 100%)";
+  }
 }
